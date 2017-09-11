990a5ffe79c8323b69e1e760a763054e4b393d9a
Zachary Aten <z_man@Zacharys-MacBook-Pro.local>
2017-09-11 09:45:47 -0400

    finished exercises

---
+++ variables_for_numbers.js
@@ -0,0 +1,13 @@
+var weight = 150;
+
+weight + 25;
+
+var originalNumber = 23;
+var numToBeAdded = 7;
+var newNum = originalNumber + numToBeAdded;
+
+var originalNum2 = 90;
+var originalNum2 = originalNum2 + 10;
+
+
+
