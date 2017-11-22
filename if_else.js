805a32a42b21a267e9cad00bd872959298ea8b26
Zachary Aten <z_man@Zacharys-MacBook-Pro.local>
2017-11-22 13:44:23 -0500

    completed some exercises

---
+++ if_else.js
@@ -0,0 +1,31 @@
+var x = prompt("what does a dog wag?");
+
+if ( x === "tail" ) {
+    alert("Correct");
+} else {
+    alert("Wrong answer");
+}
+
+
+
+var correctAnswer = "tail";
+
+if (x === correctAnswer)[
+    alert("Correct");
+] else {
+    score--;
+    askAgain = "yes";
+    alert("Incorrect");
+}
+
+var correctAnswer = "tail";
+if ( x === correctAnswer) {
+    alert("Correct");
+} else if (x==="tale") {
+    alert("Close but no");
+} else {
+    alert("Incorrect");
+}
+
+
+
---
+++ prompts.js
@@ -0,0 +1,5 @@
+var spec = prompt("your specieis?", "human");
+
+var question = "Your species?";
+var defaultAnswer = "human";
+var spec2 = prompt(question, defaultAnswer);
